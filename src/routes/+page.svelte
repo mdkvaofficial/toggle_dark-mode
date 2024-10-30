15a3ec5c7fa178f2419f10dd587e5e10098a7d6b
mdkvaofficial <mdanyalkayani.MDK@gmail.com>
2024-10-30 19:20:51 +0500

    basic dark mode toggle complete

---
+++ src/routes/+page.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	let isDarkMode = false;
+	onMount(() => {
+		isDarkMode = localStorage.getItem('dark-mode') === 'true';
+		updateDarkMode();
+	});
+	function toggleDarkMode() {
+		isDarkMode = !isDarkMode;
+		localStorage.setItem('dark-mode', isDarkMode.toString());
+		updateDarkMode();
+	}
+	function updateDarkMode() {
+		document.body.classList.toggle('dark-mode', isDarkMode);
+	}
+</script>
+
+<div>
+	<h1>May Allah Almighty Bless You. Ameen!</h1>
+	<button type="button" on:click={toggleDarkMode}>
+		{isDarkMode ? 'Switch to Light Mode' : 'Switch to Dark Mode'}
+	</button>
+</div>
+<style>
+	* {
+		margin: 0;
+		padding: 0;
+		font-family: Arial, Helvetica, sans-serif;
+	}
+	:global(body) {
+		background-color: white;
+		color: black;
+		transition:
+			background-color 0.3s,
+			color 0.3s;
+	}
+	:global(body.dark-mode) {
+		background-color: black;
+		color: white;
+	}
+	button {
+		margin-top: 20px;
+		padding: 10px 20px;
+		cursor: pointer;
+		font-size: 1em;
+		background-color: red;
+		color: white;
+	}
+	button:hover {
+		background-color: rgba(255, 0, 0, 0.344);
+		border: blue;
+	}
+</style>
